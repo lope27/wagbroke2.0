4660db2ad9250a8685c84b03b20ac82c4bfd66fa
lope27 <107971960+lope27@users.noreply.github.com>
2022-06-22 10:06:39 -0400

    Add files via upload

---
+++ minting-dapp/src/scripts/lib/NftContractType.ts
@@ -0,0 +1,7 @@
+// The name below ("WAGBROKEV2") should match the name of your Solidity contract.
+// It can be updated using the following command:
+// yarn rename-contract NEW_CONTRACT_NAME
+// Please DO NOT change it manually!
+import { WAGBROKEV2 as NftContractType } from '../../../../smart-contract/typechain/index';
+
+export default NftContractType;
